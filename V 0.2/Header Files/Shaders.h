e2fcf57b122d819d1ffc81ae33d623982c4c10fb
GitEulerAngles <62534632+GitEulerAngles@users.noreply.github.com>
2024-10-01 15:39:18 -0500

    Create Shaders.h

---
+++ V 0.2/Header Files/Shaders.h
@@ -0,0 +1,49 @@
+#ifndef SHADERS
+#define SHADERS
+
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+void getOpenGLError(const std::string& prompt);
+struct Texture {
+    std::string file;
+    int width, height, nrChannels;
+    unsigned int textureID;
+    void updateTexture();
+};
+
+struct Shaders {
+    GLuint vertexShader, fragmentShader, shaderProgram;
+    std::string vertexShaderSource, fragmentShaderSource;
+
+    void defineShader(std::string *source, std::string path);
+    void loadShaderProgram();
+};
+class Camera {
+public:
+    glm::vec3 cameraPos = glm::vec3(0.0f, 20.0f, 0.0f);
+    glm::vec3 upDirection = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraDirection, cameraRight;
+    glm::mat4 model, view, projection;
+    void mouse_callback(GLFWwindow* window);
+    void updateCamera(Shaders* shader);
+    bool isChunkVisible(glm::vec2 chunkPos);
+private:
+    float lastX = 800.0f / 2.0;
+    float lastY = 600.0f / 2.0;
+    float yaw = 0, pitch = 0;
+};
+
+#endif SHADERS
